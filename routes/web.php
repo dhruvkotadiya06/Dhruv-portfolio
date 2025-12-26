b05a36198635e24586ee1177704f8a80685ed57e
dhruvkotadiya06 <dhruvkotadiya111@email.com>
2025-12-26 10:52:23 +0530

    created about us page

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::view('/', 'pages.index')->name('home');
+Route::view('/about-us', 'pages.about-us')->name('about-us');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
